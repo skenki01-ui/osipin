159a35fcdda8c84c5c831ca220909bfb27ec9325
はち <skenki01@gmail.com>
2026-04-12 15:45:28 +0900

    fix duplicate import

--- src/pages/Pay.jsx
+++ src/pages/Pay.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../lib/supabase";
