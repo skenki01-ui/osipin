f3066aca40eb84cdc13c0aa6f02a15db527c2c37
はち <skenki01@gmail.com>
2026-04-12 13:17:10 +0900

    fix router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";  // ⭐これ追加
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
