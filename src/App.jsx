9a43be9e906c325add8baa7add40b04d477af1aa
はち <skenki01@gmail.com>
2026-04-12 13:14:03 +0900

    fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Pin from "./pages/Pin";
 
+import Terms from "./pages/legal/Terms";
+import Tokushoho from "./pages/legal/Tokushoho";
+import Privacy from "./pages/legal/Privacy";
+import Contact from "./pages/legal/Contact";
+
 export default function App(){
   return(
-    <BrowserRouter>
-      <Routes>
+    <Routes>
 
-        <Route path="/" element={<Register />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/pin" element={<Pin />} />
+      <Route path="/" element={<Register />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/pin" element={<Pin />} />
 
-      </Routes>
-    </BrowserRouter>
+      <Route path="/terms" element={<Terms />} />
+      <Route path="/tokushoho" element={<Tokushoho />} />
+      <Route path="/privacy" element={<Privacy />} />
+      <Route path="/contact" element={<Contact />} />
+
+    </Routes>
   );
 }
