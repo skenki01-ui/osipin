5d9c491e323b2944ddd88855fe74bf7073328571
はち <skenki01@gmail.com>
2026-04-12 13:39:47 +0900

    fix supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://uwbvphrrgntyqkxujsez.supabase.co";
+const supabaseAnonKey = "sb_publishable_wpMruzZPWZ9ZBoIZMR4ZVw_2Xs5t3fe";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
