d735b5d2d9305c4a6583d5f02207977d3c79be93
はち <skenki01@gmail.com>
2026-04-25 18:57:17 +0900

    test home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,3 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-
 export default function Home() {
-  const navigate = useNavigate();
-  const [point, setPoint] = useState<number>(0);
-
-  useEffect(() => {
-    const saved = localStorage.getItem("point");
-
-    if (!saved) {
-      localStorage.setItem("point", "100");
-      setPoint(100);
-    } else {
-      setPoint(Number(saved));
-    }
-  }, []);
-
-  return (
-    <div>
-      <h1>ひきよせ</h1>
-
-      <div>ポイント：{point}p</div>
-
-      <button onClick={() => navigate("/result")}>
-        今日の運勢を見る
-      </button>
-    </div>
-  );
+  return <div>強制表示OK</div>;
 }
