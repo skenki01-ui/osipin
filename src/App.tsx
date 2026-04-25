90f49f9e8001ad284c8ed8be439d44d108b43029
はち <skenki01@gmail.com>
2026-04-25 17:19:47 +0900

    debug minimal

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Result from "./pages/Result";
+
 export default function App() {
-  return <div>OK</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/result" element={<Result />} />
+    </Routes>
+  );
 }
