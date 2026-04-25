a42a32225b062c7cd08a65eed1841c6a6dc0bef6
はち <skenki01@gmail.com>
2026-04-25 17:32:27 +0900

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+
+function Test() {
+  return <div>表示テストOK</div>;
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Test />
   </React.StrictMode>
 );
