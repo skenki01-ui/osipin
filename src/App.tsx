06bb0eb4e5f14a5ee37a1a12f227a169b9facafd
はち <skenki01@gmail.com>
2026-04-25 17:25:10 +0900

    force debug home only

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
-import { Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home";
-import Result from "./pages/Result";
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/result" element={<Result />} />
-    </Routes>
-  );
+  return <Home />;
 }
